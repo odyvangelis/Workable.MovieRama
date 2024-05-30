85f80f13d648b8a163f023bb7be15599b71d8fa0
Odysseas Vangelis <odyvangelis@gmail.com>
2024-05-30 20:32:27 +0300

    init movierama project

---
+++ src/app/MovieRama.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MovieRama.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
